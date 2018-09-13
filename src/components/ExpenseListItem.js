e520240fdfadad9aeb8fc68420e6af7a10e76341
lzkmylz <lzkmylz@163.com>
2018-09-13 11:26:35 +0800

    ignored coverage filefolder and format amount and createdAt in ExpenlistItem.js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <p>description: {description}</p>
         </Link>
-        <p>amount: {amount}</p>
-        <p>createdAt: {createdAt}</p>
+        <p>amount: {numeral(amount).format('$0,0.00')}</p>
+        <p>createdAt: {moment(createdAt).format('YYYY-MM-DD')}</p>
     </div>
 );
 
